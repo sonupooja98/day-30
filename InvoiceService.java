5c92e83287cc7f25773852ad71a4178145723afd
sonupooja <sonupooja2398@gmail.com>
2021-11-07 21:42:44 +0530

    Added:message

--- InvoiceService.java
+++ InvoiceService.java
@@ -2,21 +2,28 @@ package com.day30;
 
 public class InvoiceService {
 	private static final double COST_PER_KM = 10;
-	private static final double COST_PER_MIN = 1;
-	private static final double MIN_FARE = 5.0;
+    private static final double COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
 
-	public double CalculateFare(double distance, int time) {
-		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
-		return Math.max(totalFare, MIN_FARE);
-	}
+    public double calculateFare(double distance, int time) {
 
-	public double calculateFare(Ride[] rides) {
-		double totalFare = 0.0;
-		for (Ride ride : rides) {
-			totalFare = CalculateFare(ride.getDistance(), ride.getTime());
-		}
-		return totalFare;
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
+    }
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare = calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
 
-	}
+    }
+    public InvoiceSummary calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
 
 }
